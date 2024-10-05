b71bcf533231e1e6c6158649cbcc8b52104f8753
ihsan6133 <mihsan6133@gmail.com>
2024-10-05 23:40:22 +0400

    Beginning video, other pages, local storages

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,39 @@ import * as renderer from './rendering.js'
 const main_page = document.querySelector('.main-page');
 const landing_page = document.querySelector('.landing-page');
 const play_button = document.querySelector('.play-button');
-
+const main_video = document.querySelector('.main-video');
 
 main_page.appendChild( renderer.renderer.domElement );
 
 events.init_resize_event(renderer.renderer, renderer.camera);
 events.init_pointer_move_event(renderer.on_pointer_move);
 
+if (localStorage.getItem('has_seen_intro')) {
+    landing_page.style.display = 'none';
+    main_page.style.visibility = 'visible';
+    renderer.init();
+    renderer.run();
+} else {
+    landing_page.style.display = 'flex';
+}
+
+
 play_button.addEventListener('click', (event)=>{
+    main_video.style.display = 'block';
+    main_video.play();
+
+})
+
+main_video.addEventListener('ended', (event)=>{
     landing_page.style.opacity = 0;
-    setTimeout(() => {
+        setTimeout(() => {
         landing_page.style.display = 'none';
     }, 1000);
 
     main_page.style.visibility = 'visible';
     renderer.init();
     renderer.run();
+
+    localStorage.setItem('has_seen_intro', true);
+
 })
